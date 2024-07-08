db80466544750705b1450b41c635c74c7e4e1bb6
Liubov-Pysana <144674433+Liubov-Pysana@users.noreply.github.com>
2024-07-08 16:51:07 +0100

    modal window is opening

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,40 +1,41 @@
-//import React, { useState } from "react";
+import React from "react";
 import * as Yup from "yup";
 import styles from "./SearchBar.module.css";
 import { Field, Form, Formik } from "formik";
 import toast, { Toaster } from "react-hot-toast";
 
-// const validationSchema = Yup.object().shape({
-//     query: Yup.string().min(3, "Minimum 3 letters").required("This field is required"),
-// });
+const validationSchema = Yup.object().shape({
+    query: Yup.string().min(3, "Minimum 3 letters").required("This field is required"),
+});
 
 export default function SearchBar({ onSubmit }) {
+    const handleSubmit = (values, actions) => {
+        const trimmedQuery = values.query.trim();
+        if (trimmedQuery === "") {
+            toast.error("Cannot be empty");
+        } else if (trimmedQuery.length < 3) {
+            toast.error("Minimum 3 letters");
+        } else {
+            onSubmit(trimmedQuery);
+            actions.resetForm();
+        }
+    };
+
     return (
         <header className={styles.header}>
-            <Formik
-                initialValues={{ query: "" }}
-                //validationSchema={validationSchema}
-                onSubmit={(values, actions) => {
-                    onSubmit(values.query);
-                    actions.resetForm();
-                    const trimmedQuery = values.query.trim();
-                    if (trimmedQuery === "") {
-                        toast.error("Cannot be empty");
-                    } else if (trimmedQuery.length < 3) {
-                        toast.error("Minimum 3 letters");
-                    } else {
-                        onSubmit(values.query);
-                        actions.resetForm();
-                    }
-                }}
-            >
+            <Formik initialValues={{ query: "" }} validationSchema={validationSchema} onSubmit={handleSubmit}>
                 {({ errors, touched }) => (
                     <Form className={styles.form}>
-                        <Field className={styles.input} type="text" name="query" />
+                        <Field
+                            className={`${styles.input} ${errors.query && touched.query ? styles.inputError : ""}`} // Apply error class if validation fails
+                            type="text"
+                            name="query"
+                            placeholder="Search images and photos"
+                        />
                         <button type="submit" className={styles.button}>
                             Search
                         </button>
-                        {errors.query && touched.query ? <div className="{styles.error}>{errors.query}"></div> : null}
+                        {errors.query && touched.query ? <div className={styles.error}>{errors.query}</div> : null}
                     </Form>
                 )}
             </Formik>
